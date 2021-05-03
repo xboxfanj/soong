79c915704f81ed4adf20fefa75238bb8e81a448d
Jake Weinstein <jake@aospa.co>
2021-05-03 08:47:40 -0500

    Revert "Add support for VX_AUDIO_FORMAT_LC3 in system libs."
    This reverts commit 281ce38723efbcba434835e9eb44a364abf8a825.

--- cc/config/vndk.go
+++ cc/config/vndk.go
@@ -47,5 +47,4 @@ var VndkMustUseVendorVariantList = []string{
 	"libstagefright_xmlparser",
 	"libui",
 	"libxml2",
-	"libmedia_helper",//Remove it from the list once the workaround patch is cleared in S
 }
